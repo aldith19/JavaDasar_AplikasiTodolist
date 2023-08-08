6bf9c105d0c517e3d003741e71682b555720ef55
aldith19 <aldith.reynaldi23@gmail.com>
2023-08-08 15:57:16 +0700

    initial version

--- src/AplikasiTodolist.java
+++ src/AplikasiTodolist.java
@@ -2,6 +2,31 @@ public class AplikasiTodolist {
 
     public static String[] model = new String[10];
     public static void main(String[] args) {
+    }
+
+
+    /**
+     * Menampilkan TodoList
+     */
+    public static void showTodoList(){
 
     }
+
+    /**
+     * Menambah TodoList
+     */
+    public static void addTododoList(){
+
+
+
+    }
+
+    /**
+     * Menghapus TodoList
+     */
+    public static void removeTodoList(){
+
+
+    }
+
 }
